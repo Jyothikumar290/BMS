af88a0c9d0889447c6dc28bf6f63b243a8d2fa5a
Jyothi Kumar <jyothikumar@Jyothis-MBP.bbrouter>
2023-03-14 12:30:37 +0530

    Updated name

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
 import Content from './components/Content';
 import Header from './components/Header';
+import Hello from './components/Hello';
 import Nav from './components/Nav';
+
 function App() {
   return (
     <div>
       <Header/>
       <Nav/>
       <Content/>
+      <Hello/>
     </div>
   )
 }
